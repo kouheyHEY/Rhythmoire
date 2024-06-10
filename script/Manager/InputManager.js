6e4b16302cfe5affeca2c9f3c6a9d2a52ad3e43c
kouHEY <51882041+kouheyHEY@users.noreply.github.com>
2024-06-10 23:31:14 +0900

    InputManager.js を更新

--- script/Manager/InputManager.js
+++ script/Manager/InputManager.js
@@ -1,5 +1,14 @@
 class InputManager {
     constructor(scene) {
         this.scene = scene;
+        this.keyMap = {};
+    }
+    
+    addKeyCodes(keyCode) {
+        this.keyMap[keyCode] = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes[keyCode]);
+    }
+    
+    getPushedKeyOf(keyCode) {
+        return this.keyMap[keyCode].isDown;
     }
 }
