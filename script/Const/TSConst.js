d002fa4fe7f089d5da693bc09105cd00c76ce3dd
kouheyHEY <vudtarm@gmail.com>
2024-07-01 22:06:34 +0900

    update titleScene

---
+++ script/Const/TSConst.js
@@ -0,0 +1,24 @@
+const C_TS = {
+    /** モードの表示用文字列 階段 */
+    MODE_DISP_STR_STAIR: 'かいだん',
+    /** モードの表示用文字列 らせん */
+    MODE_DISP_STR_SPIRAL: 'らせん',
+    /** モードの表示用文字列 ランダム */
+    MODE_DISP_STR_RANDOM: 'らんだむ',
+
+    /** 文字列 有効 */
+    STR_VALID: 'ゆうこう',
+    /** 文字列 無効 */
+    STR_INVALID: 'むこう',
+
+    /** 項目名 モード */
+    COL_STR_MODE: 'もーど',
+    /** 項目名 同時押し */
+    COL_STR_MULTI: 'どうじおし',
+    /** 項目名 レーン数 */
+    COL_LANE_NUM: 'れーんのかず',
+    /** 項目名 同時押し有効レーン数 */
+    COL_MULTI_LANE_NUM: 'どうじおしゆうこうすう',
+
+
+};
