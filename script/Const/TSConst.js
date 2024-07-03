eda0b8b71c89fec7caabbdeb4db4e1e4a3d0ba82
kouheyHEY <vudtarm@gmail.com>
2024-07-03 23:44:42 +0900

    update titleScene

--- script/Const/TSConst.js
+++ script/Const/TSConst.js
@@ -16,9 +16,26 @@ const C_TS = {
     /** 項目名 同時押し */
     COL_STR_MULTI: 'どうじおし',
     /** 項目名 レーン数 */
-    COL_LANE_NUM: 'れーんのかず',
+    COL_STR_LANE_NUM: 'れーんのかず',
     /** 項目名 同時押し有効レーン数 */
-    COL_MULTI_LANE_NUM: 'どうじおしゆうこうすう',
+    COL_STR_MULTI_LANE_NUM: 'さいだいどうじおし',
 
+    /** 表示用文字列 スタートボタン */
+    DISP_STR_START: 'START',
+
+    /** 表示位置 タイトル */
+    DISP_POS_TITLE_Y: 100,
+    /** 表示位置 スタートボタン */
+    DISP_POS_START_Y: C_COMMON.D_HEIGHT - 100,
+
+    /** 表示位置 1列目 */
+    DISP_POS_COL1: C_COMMON.D_WIDTH / 3,
+    /** 表示位置 2列目 */
+    DISP_POS_COL2: C_COMMON.D_WIDTH * 2 / 3,
+
+    /** 表示位置 初期Y座標 */
+    DISP_POS_INIT_Y: 220,
+    /** 表示位置 垂直間隔 */
+    DISP_POS_SPAN_VERT: 100,
 
 };
--- script/Scene/TitleScene.js
+++ script/Scene/TitleScene.js
@@ -19,15 +19,71 @@ class TitleScene extends Phaser.Scene {
         }
         */
 
-        this.add.text(C_COMMON.D_WIDTH / 2, 300, C_COMMON.GAME_TITLE,
-            { fontSize: C_COMMON.FONT_SIZE_LARGE, fontFamily: C_COMMON.FONT_FAMILY_BIT12_BOLD, fill: '#fff' })
-            .setOrigin(0.5);
+        // 表示文字列の縦方向間隔
+        let dispRow = 0;
+        let dispInitY = C_TS.DISP_POS_INIT_Y;
 
-        const startButton = this.add.text(C_COMMON.D_WIDTH / 2, 400, 'Start',
-            { fontSize: C_COMMON.FONT_SIZE_SMALL, fontFamily: C_COMMON.FONT_FAMILY_BIT12, fill: '#fff' })
-            .setOrigin(0.5);
+        this.add.text(
+            C_COMMON.D_WIDTH / 2, C_TS.DISP_POS_TITLE_Y, C_COMMON.GAME_TITLE,
+            {
+                fontSize: C_COMMON.FONT_SIZE_LARGE,
+                fontFamily: C_COMMON.FONT_FAMILY_BIT12_BOLD,
+                fill: '#fff'
+            }).setOrigin(0.5);
+
+        const startButton = this.add.text(
+            C_COMMON.D_WIDTH / 2, C_TS.DISP_POS_START_Y, C_TS.DISP_STR_START,
+            {
+                fontSize: C_COMMON.FONT_SIZE_SMALL,
+                fontFamily: C_COMMON.FONT_FAMILY_BIT12,
+                fill: '#fff'
+            }).setOrigin(0.5);
+
+        // スタートボタンを有効化
         startButton.setInteractive();
 
+        // モードを表示
+        const colModeText = this.add.text(
+            C_TS.DISP_POS_COL1, dispInitY + dispRow * C_TS.DISP_POS_SPAN_VERT,
+            C_TS.COL_STR_MODE,
+            {
+                fontSize: C_COMMON.FONT_SIZE_SMALL,
+                fontFamily: C_COMMON.FONT_FAMILY_BIT12,
+                fill: '#fff'
+            }).setOrigin(0.5);
+
+        // 同時押しモードを表示
+        const colMultiText = this.add.text(
+            C_TS.DISP_POS_COL2, dispInitY + dispRow * C_TS.DISP_POS_SPAN_VERT,
+            C_TS.COL_STR_MULTI,
+            {
+                fontSize: C_COMMON.FONT_SIZE_SMALL,
+                fontFamily: C_COMMON.FONT_FAMILY_BIT12,
+                fill: '#fff'
+            }).setOrigin(0.5);
+
+        dispRow++;
+        // レーン数を表示
+        const colLaneNum = this.add.text(
+            C_TS.DISP_POS_COL1, dispInitY + dispRow * C_TS.DISP_POS_SPAN_VERT,
+            C_TS.COL_STR_LANE_NUM,
+            {
+                fontSize: C_COMMON.FONT_SIZE_SMALL,
+                fontFamily: C_COMMON.FONT_FAMILY_BIT12,
+                fill: '#fff'
+            }).setOrigin(0.5);
+
+        // 同時押し有効レーン数を表示
+        const colMultiLaneNumText = this.add.text(
+            C_TS.DISP_POS_COL2, dispInitY + dispRow * C_TS.DISP_POS_SPAN_VERT,
+            C_TS.COL_STR_MULTI_LANE_NUM,
+            {
+                fontSize: C_COMMON.FONT_SIZE_SMALL,
+                fontFamily: C_COMMON.FONT_FAMILY_BIT12,
+                fill: '#fff'
+            }).setOrigin(0.5);
+
+        // 押下時ゲームシーンに遷移
         startButton.on('pointerdown', () => {
             this.scene.start(C_COMMON.SCENE_GAMESCENE);
         });
